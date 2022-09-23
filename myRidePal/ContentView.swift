873b1b30205c9c564cf5319bc8dfe1e1c4068772
Long Hoang <longhoangdev21@gmail.com>
2022-09-23 16:13:01 -0400

    Initial Commit

---
+++ myRidePal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myRidePal
+//
+//  Created by Long Hai on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myRidePal/myRidePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  myRidePalApp.swift
+//  myRidePal
+//
+//  Created by Long Hai on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct myRidePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
